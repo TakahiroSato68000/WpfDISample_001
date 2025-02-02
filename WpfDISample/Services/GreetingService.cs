49cd1683e21cd37715f45b16d05f9b7fc1540666
Takahiro Sato <takax68000@gmail.com>
2025-02-02 11:26:03 +0900

    - DI

---
+++ WpfDISample/Services/GreetingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDISample.Services
+{
+    public class GreetingService : IGreetingService
+    {
+        public string Greet(string name)
+        {
+            return $"Hello, {name}!";
+        }
+    }
+}
